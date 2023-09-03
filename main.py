823e7f2f4933d7583003644fb9c769ccfcb2756e
blackhearrt <svmamonov36@gmail.com>
2023-09-03 13:17:46 +0300

    First version of code

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
+    users = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+    }
+    current_day = date.today().weekday()
+    current_date = date.today()
+    monday = current_date - timedelta(days=current_day)
+    for user in users:
+        user_birthday = user["birthday"]
+        user_birthday_this_year = date(
+            current_date.year, user_birthday.month, user_birthday.day
+        )
+        if user_birthday_this_year < current_date:
+            user_birthday_this_year = date(
+                current_date.year + 1, user_birthday.month, user_birthday.day
+            )
+        days_until_birthday = (user_birthday_this_year - current_date).days
+        if 0 <= days_until_birthday <= 4:
+            day_of_week = (monday + timedelta(days_until_birthday)).strftime("%A")
+            users[day_of_week].append(user["name"])
+
     return users
 
 
